b509b317f9e1ff736ee2e8ce1ab0218b5c211f6b
starboycmg <140527992+starboycmg@users.noreply.github.com>
2024-05-29 20:09:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/about', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(Server is running on http://localhost:${port});
 });
