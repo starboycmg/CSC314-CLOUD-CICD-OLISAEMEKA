6b063f566ede614e1779c059a34593b587cb2842
starboycmg <140527992+starboycmg@users.noreply.github.com>
2024-05-29 22:32:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define the root route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Define the '/about' route
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/about.html'));
+    res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. MOFUNANYA CHRISTOPHER VUG/CSC/21/5659');
 });
 
-
-
+// Start the server
 app.listen(port, () => {
-    console.log(Server is running on http://localhost:${port});
+    console.log(`Server is running on http://localhost:${port}`);
 });
